00bcb363e7adf2f794112a2036480150f5b3c07c
Teddy <brianrcdteodoro@gmail.com>
2019-12-03 15:16:00 +0800

    state and lifecycle

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+class App extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = { date: new Date() };
+	}
+
+	componentDidMount() {
+		this.timerID = setInterval(() => this.tick(), 1000);
+	}
+
+	componentWillUnmount() {
+		clearInterval(this.timerID);
+	}
+
+	tick() {
+		this.setState({
+			date: new Date()
+		});
+	}
+
 	render() {
-		return <div className="App" />;
+		return (
+			<div>
+				<h1>Hello World!</h1>
+				<h2>It is {this.state.date.toLocaleTimeString()}</h2>
+			</div>
+		);
 	}
 }
 
